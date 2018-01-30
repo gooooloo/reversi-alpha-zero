d7044f07390a7a3bcc3c3bfac9b65f42ba30c678
gooooloo <qidu.lin@gmail.com>
2018-01-30 23:20:16 +0800

    update ntest script

--- batch.ntest.sh
+++ batch.ntest.sh
@@ -1,9 +1,9 @@
 log=/tmp/batch.ntest.sh.result.log
 echo '' > $log
-for ndepth in `echo 12 13`; do
-	for nsteps in `echo 424000`; do
+for ndepth in `echo 10 11 12 13`; do
+	for nsteps in `echo 386400 391200 401600 406400`; do
 		for nsims in `echo 100 200 400 800`; do
-			cmd=`echo python -m src.reversi_zero.run elo_p1_ntest --env reversi --n-workers 4 --n-games 20 --ntest-depth $ndepth --n-sims $nsims --n-steps-model $nsteps --save-versus-dir /tmp/`
+			cmd=`echo python -m src.reversi_zero.run elo_p1_ntest --env reversi --n-workers 4 --n-games 10 --ntest-depth $ndepth --n-sims $nsims --n-steps-model $nsteps --save-versus-dir ./data/reversi/ggf/`
 			echo $cmd
 			echo $cmd >> $log
 			eval $cmd >> $log
